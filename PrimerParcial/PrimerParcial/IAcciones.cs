cb5a128083ae3324604076145fdb9eb42f5661d8
DeCost96 <decost96@gmail.com>
2021-01-25 12:13:32 -0500

    Add files via upload

---
+++ PrimerParcial/PrimerParcial/IAcciones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PrimerParcial
+{
+    internal interface IAcciones
+    {
+        List<string> pasaje;
+        internal void ComprarPasaje(Vuelo vuelo, int cantidad, string destino)
+        {
+            if (vuelo.AsientosLibres(destino) >= cantidad)
+            {
+                pasaje = vuelo.Descargar(destino, cantidad);
+            }
+        }
+    }
+}
